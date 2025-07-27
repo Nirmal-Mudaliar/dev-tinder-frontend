d7e240e89db908e9df90b634aca5a1de61a851cf
NirmalMudaliar <nmudaliar@valorx.com>
2025-07-27 19:25:59 +0530

    chore: setup initial project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
+import { PRIME_NG_THEME } from '../styles/theme/dev-tinder-theme';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: 
+  [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: PRIME_NG_THEME
+            }
+        })
+  ]
 };
---
+++ src/app/core/components/signup/signup.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  imports: [],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SignupComponent {
+
+}
