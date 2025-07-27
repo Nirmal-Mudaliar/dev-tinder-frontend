6e328962b3fdb4ca436ff6c0c95d8a3135506422
NirmalMudaliar <nmudaliar@valorx.com>
2025-07-27 21:56:06 +0530

    feat(app): create home component

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    SideBarComponent,
+  ]
+})
+export class HomeModule { }
